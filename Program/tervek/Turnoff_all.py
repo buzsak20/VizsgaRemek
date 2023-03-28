2591b0094e545222e9099120100b75f403d82504
buzsak20 <norman.buzsak@gmail.com>
2023-03-28 20:42:04 +0200

    2023-03-28 Program

--- Program/tervek/Turnoff_all.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-with open('gepnev.txt', encoding='utf8') as f:
-    for line in f:
-        command_logoff = ['powershell','Invoke-Command -ComputerName "{}" -ScriptBlock { logoff 2 }'.format(line.strip())]
-        command_turnoff = ['powershell','Stop-Computer -ComputerName "{}"'.format(line.strip())]
-        subprocess.run(command_logoff, command_turnoff, shell=True)
