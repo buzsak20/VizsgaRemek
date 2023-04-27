636bd36135257286581dcda13d410a2564b0fb9f
buzsak20 <norman.buzsak@gmail.com>
2023-04-27 15:42:16 +0200

    2023-04-27 Konyvtar szerkezet egyszerusitese

--- Program/Working/Ping_all.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-with open('ip.txt', encoding='utf8') as f:
-    for line in f:
-        ping = ['powershell','ping {}'.format(line.strip())]
-        subprocess.run(ping, shell=True)
--- Program/Working/Working_turnoff.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-command_connect  = ['powershell', 'Enter-PSSession TESZT']
-command_logoff = ['powershell','Invoke-Command -ComputerName TESZT -ScriptBlock { logoff console }']
-command_turnoff = ['powershell','Invoke-Command -ComputerName TESZT -ScriptBlock { Stop-Computer }']
-subprocess.run(command_connect, shell=True)
-subprocess.run(command_logoff, shell=True)
-subprocess.run(command_turnoff, shell=True)
-
